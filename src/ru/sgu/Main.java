9d9dd20cf217cc7781c31d3b61346fe91e5a3580
TaranovAV <2001taranov@gmail.com>
2024-04-03 14:43:40 +0400

    Обновлен .gitignore

--- src/ru/sgu/Main.java
+++ src/ru/sgu/Main.java
@@ -1,42 +1,56 @@
 package ru.sgu;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("""
-                    Задание 1\s
-                    1) Вывод Hello World\s
-                    2) Операции между двумя большими числами\s
-                    3) Ввод/вывод из файла\s
-                    4) Выход из программы""");
+                Задание 4\s
+                1) Задача с датами\s
+                2) Задача с днями недели\s
+                3) Задача со строкой\s
+                4) Выход из программы""");
 
         Scanner in = new Scanner(System.in);
+        System.out.print("Введите число от 1 до 3 для выбора задания, 4 для выхода:");
 
         while (true) {
             System.out.println();
-            System.out.println("Введите число от 1 до 3 для выбора задания, 4 для выхода:");
-            String choice = in.next();
+            if (in.hasNext()) {
+                String choice = in.next();
 
-            switch (choice) {
-                case "1":
-                    Task1 task1 = new Task1();
-                    task1.start();
-                    break;
-                case "2":
-                    Task2 task2 = new Task2();
-                    task2.start();
-                    break;
-                case "3":
-                    Task3 task3 = new Task3();
-                    task3.start();
-                    break;
-                case "4":
-                    System.out.println("Выход...");
-                    in.close();
-                    return;
-                default:
-                    System.out.println("Неверный ввод.");
+                switch (choice) {
+                    case "1":
+                        Task1 task1 = new Task1();
+                        task1.start();
+                        break;
+                    case "2":
+                        System.out.println("Введите день недели и число:");
+                        try (Scanner scanner = new Scanner(System.in)) {
+                            String weekday = scanner.next().toLowerCase();
+                            int addDays = scanner.nextInt();
+                            Task2 task2 = new Task2(weekday, addDays);
+                            task2.start();
+                        } catch (NoSuchElementException | IllegalArgumentException e) {
+                            System.out.println(e.getMessage());
+                            System.exit(0);
+                        }
+                        break;
+                    case "3":
+                        Task3 task3 = new Task3();
+                        task3.start();
+                        return;
+                    case "4":
+                        System.out.println("Выход...");
+                        return;
+                    default:
+                        System.out.println("Неверный ввод. Попробуйте снова.");
+                }
+            } else {
+                System.out.println("Завершение программы.");
+                in.close();
+                return;
             }
         }
     }
